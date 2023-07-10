78814f2799e922e3e32946a3b0066379a9a5b83a
jxxnixx <dkepffk10@sookmyung.ac.kr>
2023-07-11 00:24:42 +0900

    .
    - 2022/07/20
    - nextjs를 활용한 간단한 최신 영화 사이트

--- next.config.js
+++ next.config.js
@@ -1,7 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const API_KEY = "b027be29d983f35a80fb316b6734e898";
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-sexy-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/:id",
+        destination: `https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
